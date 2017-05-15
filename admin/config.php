1961184b568deb7bb9a3ebc0b4848478de91a846
oleh1 <lyboleg@gmail.com>
2017-05-15 19:05:52 +0200

    1

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://premiumsvet.sumdu.pp.ua/admin/');
+define('HTTP_CATALOG', 'http://premiumsvet.sumdu.pp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://premiumsvet.sumdu.pp.ua/admin/');
+define('HTTPS_CATALOG', 'http://premiumsvet.sumdu.pp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/premiumsvet.sumdu.pp.ua/admin/');
+define('DIR_SYSTEM', '/var/www/premiumsvet.sumdu.pp.ua/system/');
+define('DIR_IMAGE', '/var/www/premiumsvet.sumdu.pp.ua/image/');
+define('DIR_LANGUAGE', '/var/www/premiumsvet.sumdu.pp.ua/admin/language/');
+define('DIR_TEMPLATE', '/var/www/premiumsvet.sumdu.pp.ua/admin/view/template/');
+define('DIR_CONFIG', '/var/www/premiumsvet.sumdu.pp.ua/system/config/');
+define('DIR_CACHE', '/var/www/premiumsvet.sumdu.pp.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/premiumsvet.sumdu.pp.ua/system/storage/download/');
+define('DIR_LOGS', '/var/www/premiumsvet.sumdu.pp.ua/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/premiumsvet.sumdu.pp.ua/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/premiumsvet.sumdu.pp.ua/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/premiumsvet.sumdu.pp.ua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'l4b01991');
+define('DB_DATABASE', 'premiumsvet');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
